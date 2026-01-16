14a64fa6bad0305aea48b7fa51a8f6ccd04e9684
narodocnt <narodocnt@gmail.com>
2026-01-16 22:50:07 +0200

    Update hromadas-data.js

--- hromadas-data.js
+++ hromadas-data.js
@@ -1,10 +1,21 @@
 const hromadasGeoJSON = {
-  "type": "FeatureCollection",
-  "features": [
-    { "type": "Feature", "properties": { "name": "Звенигородська" }, "geometry": { "type": "Polygon", "coordinates": [[[30.93, 49.09], [31.05, 49.12], [31.08, 49.05], [30.95, 49.02], [30.93, 49.09]]] } },
-    { "type": "Feature", "properties": { "name": "Кам’янська" }, "geometry": { "type": "Polygon", "coordinates": [[[31.95, 49.01], [32.10, 49.05], [32.15, 48.95], [32.00, 48.92], [31.95, 49.01]]] } },
-    { "type": "Feature", "properties": { "name": "Смілянська" }, "geometry": { "type": "Polygon", "coordinates": [[[31.85, 49.20], [32.00, 49.25], [32.05, 49.15], [31.90, 49.12], [31.85, 49.20]]] } },
-    { "type": "Feature", "properties": { "name": "Тальнівська" }, "geometry": { "type": "Polygon", "coordinates": [[[30.65, 48.92], [30.80, 48.95], [30.85, 48.85], [30.70, 48.82], [30.65, 48.92]]] } },
-    { "type": "Feature", "properties": { "name": "Христинівська" }, "geometry": { "type": "Polygon", "coordinates": [[[29.90, 48.85], [30.05, 48.88], [30.10, 48.78], [29.95, 48.75], [29.90, 48.85]]] } }
-  ]
-};
+ const hromadasGeoJSON = { "type": "FeatureCollection", "features": [
+    {"type":"Feature","properties":{"name":"Черкаська"},"geometry":{"type":"Polygon","coordinates":[[[31.88,49.48],[32.12,49.52],[32.15,49.38],[31.92,49.35],[31.88,49.48]]]}},
+    {"type":"Feature","properties":{"name":"Золотоніська"},"geometry":{"type":"Polygon","coordinates":[[[31.92,49.78],[32.15,49.82],[32.18,49.65],[31.95,49.62],[31.92,49.78]]]}},
+    {"type":"Feature","properties":{"name":"Канівська"},"geometry":{"type":"Polygon","coordinates":[[[31.35,49.85],[31.60,49.88],[31.62,49.72],[31.38,49.70],[31.35,49.85]]]}},
+    {"type":"Feature","properties":{"name":"Смілянська"},"geometry":{"type":"Polygon","coordinates":[[[31.80,49.25],[32.05,49.28],[32.08,49.12],[31.82,49.10],[31.80,49.25]]]}},
+    {"type":"Feature","properties":{"name":"Кам’янська"},"geometry":{"type":"Polygon","coordinates":[[[31.95,49.08],[32.18,49.12],[32.22,48.95],[31.98,48.92],[31.95,49.08]]]}},
+    {"type":"Feature","properties":{"name":"Чигиринська"},"geometry":{"type":"Polygon","coordinates":[[[32.55,49.15],[32.80,49.18],[32.82,49.02],[32.58,49.00],[32.55,49.15]]]}},
+    {"type":"Feature","properties":{"name":"Корсунь-Шевченківська"},"geometry":{"type":"Polygon","coordinates":[[[31.15,49.48],[31.40,49.52],[31.42,49.38],[31.18,49.35],[31.15,49.48]]]}},
+    {"type":"Feature","properties":{"name":"Уманська"},"geometry":{"type":"Polygon","coordinates":[[[30.15,48.82],[30.38,48.85],[30.40,48.68],[30.18,48.65],[30.15,48.82]]]}},
+    {"type":"Feature","properties":{"name":"Звенигородська"},"geometry":{"type":"Polygon","coordinates":[[[30.85,49.12],[31.10,49.15],[31.12,48.98],[30.88,48.95],[30.85,49.12]]]}},
+    {"type":"Feature","properties":{"name":"Жашківська"},"geometry":{"type":"Polygon","coordinates":[[[30.05,49.28],[30.30,49.32],[30.32,49.15],[30.08,49.12],[30.05,49.28]]]}},
+    {"type":"Feature","properties":{"name":"Тальнівська"},"geometry":{"type":"Polygon","coordinates":[[[30.65,48.95],[30.88,48.98],[30.90,48.82],[30.68,48.80],[30.65,48.95]]]}},
+    {"type":"Feature","properties":{"name":"Христинівська"},"geometry":{"type":"Polygon","coordinates":[[[29.90,48.88],[30.12,48.92],[30.15,48.75],[29.92,48.72],[29.90,48.88]]]}},
+    {"type":"Feature","properties":{"name":"Монастирищенська"},"geometry":{"type":"Polygon","coordinates":[[[29.75,49.02],[29.98,49.05],[30.00,48.88],[29.78,48.85],[29.75,49.02]]]}},
+    {"type":"Feature","properties":{"name":"Шполянська"},"geometry":{"type":"Polygon","coordinates":[[[31.30,49.05],[31.52,49.08],[31.55,48.92],[31.32,48.90],[31.30,49.05]]]}},
+    {"type":"Feature","properties":{"name":"Городищенська"},"geometry":{"type":"Polygon","coordinates":[[[31.35,49.35],[31.58,49.38],[31.60,49.22],[31.38,49.20],[31.35,49.35]]]}},
+    {"type":"Feature","properties":{"name":"Лисянська"},"geometry":{"type":"Polygon","coordinates":[[[30.75,49.28],[30.98,49.32],[31.00,49.15],[30.78,49.12],[30.75,49.28]]]}},
+    {"type":"Feature","properties":{"name":"Чорнобаївська"},"geometry":{"type":"Polygon","coordinates":[[[32.25,49.72],[32.50,49.75],[32.52,49.58],[32.28,49.55],[32.25,49.72]]]}},
+    {"type":"Feature","properties":{"name":"Драбівська"},"geometry":{"type":"Polygon","coordinates":[[[32.05,50.05],[32.28,50.08],[32.30,49.92],[32.08,49.90],[32.05,50.05]]]}}
+]};                               
