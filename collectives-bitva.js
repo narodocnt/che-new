0a0b011e4f9872dd5a78ace3bd6e73eac15e6095
narodocnt <narodocnt@gmail.com>
2026-01-30 20:27:30 +0200

    Update collectives-bitva.js

--- collectives-bitva.js
+++ collectives-bitva.js
@@ -10,7 +10,7 @@ window.collectivesDatabase = {
         "location": "Смілянська",
         "media": "https://scontent-mia3-3.xx.fbcdn.net/v/t15.5256-10/604290490_2030897574364185_8349501958074934146_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5fad0e"
     },
-    "11": {
+    "10": {
         "key": "zveny",
         "name": "Оркестр духових інструментів",
         "institution": "Звенигородський Центр культури і дозвілля ім. Т. Шевченка",
@@ -18,7 +18,7 @@ window.collectivesDatabase = {
         "location": "Звенигородська",
         "media": "https://scontent-det1-1.xx.fbcdn.net/v/t15.5256-10/605489878_1472787384850228_7157112104133130625_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=102&ccb=1-7&_nc_sid=5fad0e"
     },
-    "12": {
+    "11": {
         "key": "kamyanka",
         "name": "Духовий ансамбль",
         "institution": "Будинок культури Кам’янської громади",
@@ -42,7 +42,7 @@ window.collectivesDatabase = {
         "location": "Христинівська",
         "media": "https://scontent-ord5-2.xx.fbcdn.net/v/t15.5256-10/611254278_1260839919431949_6855917492691376518_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=103&ccb=1-7&_nc_sid=5fad0e"
     },
-    "10": {
+    "12": {
         "key": "vodogray",
         "name": "Народний аматорський естрадно-духовий ансамбль «Водограй»",
         "institution": "Великоканівецький СБК",
