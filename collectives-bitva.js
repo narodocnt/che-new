f95a8d8c9dc3efc2610115b02bd81b2d1a8fb85b
narodocnt <narodocnt@gmail.com>
2026-01-30 09:46:00 +0200

    Update collectives-bitva.js

--- collectives-bitva.js
+++ collectives-bitva.js
@@ -1,41 +1,53 @@
 /**
- * collectives-bitva.js - Офіційний реєстр учасників
+ * collectives-bitva.js - Офіційний реєстр учасників (Версія з ID)
  */
 window.collectivesDatabase = {
-    "smila": {
+    "12": {
+        "key": "smila",
         "name": "Духовий оркестр «Божидар»",
         "institution": "Смілянська ДМШ №2",
         "leader": "Владислав Сірий",
-        "location": "Смілянська"
+        "location": "смілянська",
+        "media": "https://scontent-mia3-3.xx.fbcdn.net/v/t15.5256-10/604290490_2030897574364185_8349501958074934146_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5fad0e"
     },
-    "zveny": {
+    "10": {
+        "key": "zveny",
         "name": "Оркестр духових інструментів",
         "institution": "Звенигородський Центр культури і дозвілля ім. Т. Шевченка",
         "leader": "Володимир Дзеціна",
-        "location": "Звенигородська"
+        "location": "звенигородська",
+        "media": "https://scontent-det1-1.xx.fbcdn.net/v/t15.5256-10/605489878_1472787384850228_7157112104133130625_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=102&ccb=1-7&_nc_sid=5fad0e"
     },
-    "kamyanka": {
+    "11": {
+        "key": "kamyanka",
         "name": "Духовий ансамбль",
         "institution": "Будинок культури Кам’янської громади",
         "leader": "Володимир Кравець",
-        "location": "Кам’янська"
+        "location": "кам’янська",
+        "media": "https://scontent.fhuy2-1.fna.fbcdn.net/v/t15.5256-10/595661979_846728421312742_9031563099420240341_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5fad0e"
     },
-    "talne": {
+    "20": {
+        "key": "talne",
         "name": "Народний аматорський духовий оркестр «Сурми Тальнівщини»",
         "institution": "Центр культурних послуг Тальнівської міської ради",
         "leader": "Володимир Таран",
-        "location": "Тальнівська"
+        "location": "тальнівська",
+        "media": "https://scontent-dfw5-1.xx.fbcdn.net/v/t15.5256-10/603762538_1317445256737431_3626944906055376537_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=107&ccb=1-7&_nc_sid=5fad0e"
     },
-    "hrist": {
+    "17": {
+        "key": "hrist",
         "name": "Духовий оркестр",
         "institution": "Великосевастянівський БК (Христинівська громада)",
         "leader": "Віталій Сверблик",
-        "location": "Христинівська"
+        "location": "христинівська",
+        "media": "https://scontent-ord5-2.xx.fbcdn.net/v/t15.5256-10/611254278_1260839919431949_6855917492691376518_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=103&ccb=1-7&_nc_sid=5fad0e"
     },
-    "vodogray": {
+    "14": {
+        "key": "vodogray",
         "name": "Народний аматорський естрадно-духовий ансамбль «Водограй»",
         "institution": "Великоканівецький СБК (Чорнобаївська ТГ)",
         "leader": "Павло Лещенко",
-        "location": "Золотоніська"
+        "location": "золотоніська",
+        "media": "https://scontent-atl3-3.xx.fbcdn.net/v/t15.5256-10/599617074_4422636818000921_7785312714600574392_n.jpg?stp=dst-jpg_p720x720_tt6&_nc_cat=109&ccb=1-7&_nc_sid=5fad0e"
     }
 };
