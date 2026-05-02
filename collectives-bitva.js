4edd9241e2aae817765d59e523b0d1cbcd753216
narodocnt <narodocnt@gmail.com>
2026-05-02 20:38:09 +0300

    Update collectives-bitva.js

--- collectives-bitva.js
+++ collectives-bitva.js
@@ -1,6 +1,7 @@
 /**
- * collectives-bitva.js - Офіційний реєстр учасників (Версія з ID)
+ * collectives-bitva.js - Офіційний реєстр учасників (ОНОВЛЕНО: Локальні фото)
  */
+
 window.collectivesDatabase = {
     "10": {
         "key": "smila",
@@ -8,7 +9,8 @@ window.collectivesDatabase = {
         "institution": "Смілянська ДМШ №2",
         "leader": "Владислав Сірий",
         "location": "Смілянська",
-        "media": "https://scontent-mia3-3.xx.fbcdn.net/v/t15.5256-10/604290490_2030897574364185_8349501958074934146_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5fad0e"
+        // Пряме посилання на ваш репозиторій
+        "media": "https://narodocnt.online/img/bitva/bogidar.jpg"
     },
     "14": {
         "key": "zveny",
@@ -16,7 +18,7 @@ window.collectivesDatabase = {
         "institution": "Звенигородський Центр культури і дозвілля ім. Т. Шевченка",
         "leader": "Володимир Дзеціна",
         "location": "Звенигородська",
-        "media": "https://scontent-det1-1.xx.fbcdn.net/v/t15.5256-10/605489878_1472787384850228_7157112104133130625_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=102&ccb=1-7&_nc_sid=5fad0e"
+        "media": "https://narodocnt.online/img/bitva/zvenigorodka.jpg"
     },
     "11": {
         "key": "kamyanka",
@@ -24,7 +26,7 @@ window.collectivesDatabase = {
         "institution": "Будинок культури Кам’янської громади",
         "leader": "Володимир Кравець",
         "location": "Кам’янська",
-        "media": "https://scontent.fhuy2-1.fna.fbcdn.net/v/t15.5256-10/595661979_846728421312742_9031563099420240341_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5fad0e"
+        "media": "https://narodocnt.online/img/bitva/kamjanka.jpg"
     },
     "20": {
         "key": "talne",
@@ -32,7 +34,7 @@ window.collectivesDatabase = {
         "institution": "Центр культурних послуг Тальнівської міської ради",
         "leader": "Володимир Таран",
         "location": "Тальнівська",
-        "media": "https://scontent-dfw5-1.xx.fbcdn.net/v/t15.5256-10/603762538_1317445256737431_3626944906055376537_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=107&ccb=1-7&_nc_sid=5fad0e"
+        "media": "https://narodocnt.online/img/bitva/taljne.jpg"
     },
     "17": {
         "key": "hrist",
@@ -40,7 +42,7 @@ window.collectivesDatabase = {
         "institution": "Великосевастянівський БК (Христинівська громада)",
         "leader": "Віталій Сверблик",
         "location": "Христинівська",
-        "media": "https://scontent-ord5-2.xx.fbcdn.net/v/t15.5256-10/611254278_1260839919431949_6855917492691376518_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=103&ccb=1-7&_nc_sid=5fad0e"
+        "media": "https://narodocnt.online/img/bitva/velikosevastjyanivka.jpg"
     },
     "12": {
         "key": "vodogray",
@@ -48,6 +50,19 @@ window.collectivesDatabase = {
         "institution": "Великоканівецький СБК",
         "leader": "Павло Лещенко",
         "location": "Чорнобаївська",
-        "media": "https://scontent-atl3-3.xx.fbcdn.net/v/t15.5256-10/599617074_4422636818000921_7785312714600574392_n.jpg?stp=dst-jpg_p720x720_tt6&_nc_cat=109&ccb=1-7&_nc_sid=5fad0e"
+        "media": "https://narodocnt.online/img/bitva/vodogray.jpg"
     }
 };
+
+/**
+ * Допоміжна функція для отримання медіафайлу за ID колективу
+ * Використовуйте її в основному скрипті рейтингу
+ */
+function getBitvaPhotoById(id) {
+    const db = window.collectivesDatabase;
+    if (db[id] && db[id].media) {
+        return db[id].media;
+    }
+    // Якщо ID не знайдено, повертаємо загальну картинку
+    return "https://narodocnt.online/img/bitva/bitva-general.jpg";
+}
